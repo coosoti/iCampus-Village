f7fa58c1b2eb40cd0157617b92cfaffecb29be46
Charles Osoti <charlesosoti@gmail.com>
2017-10-10 10:29:59 +0300

    Added User roles and permissions

--- app/auth/views.py
+++ app/auth/views.py
@@ -24,7 +24,7 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
-            return redirect(request.args.get('next') or url_for('/'))
+            return redirect(request.args.get('next') or url_for('main.home'))
         flash('Incorrect email or password.')   
     return render_template('auth/login.html', form=form)
 
@@ -32,4 +32,4 @@ def login():
 def logout():
     logout_user()
     flash('You have successfully logged out.')
-    return redirect(url_for('/'))
+    return redirect('/')
